e59a1a3a582567b4ae8f4b2ac5ecd180abb0057f
Daniel Suarez Gil <dsiarez-2016582@kinal.edu.gt>
2022-05-17 23:47:13 -0600

    Se agregó las rutas del CRUD empresas y administrador

---
+++ src/routes/enterprise.routes.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express');
+const enterpriseController = require('../controllers/enterprise.controller');
+const api = express.Router();
+const mdAuth = require('../services/authenticated');
+
+//FUNCIÓN PÚBLICA
+api.get('/test', enterpriseController.test);
+
+//FUNCIONES PRIVADAS
+//ENTERPRISE
+api.post('/register', enterpriseController.register);
+api.post('/login', enterpriseController.login);
+api.put('/update/:id', mdAuth.ensureAuth, enterpriseController.update);
+api.delete('/delete/:id', mdAuth.ensureAuth, enterpriseController.delete);
+api.get('/myEnterprise', mdAuth.ensureAuth, enterpriseController.myEnterprise);
+
+
+//FUNCIONES PRIVADAS
+//ADMIN
+api.post('/saveEnterprise', [mdAuth.ensureAuth, mdAuth.isAdmin], enterpriseController.saveEnterprise);
+api.put('/updateEnterprise/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], enterpriseController.updateEnterprise);
+api.delete('/deleteEnterprise/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], enterpriseController.deleteEnterprise);
+api.get('/getEnterprises', [mdAuth.ensureAuth, mdAuth.isAdmin], enterpriseController.getEnterprises);
+api.get('/getEnterprise/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], enterpriseController.getEnterprise);
+
+
+module.exports = api; 
