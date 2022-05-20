fbe45b5091c2800f22305f47b1325f42c2a97dcf
bsolano-2020189 <bsolano-2020189@kinal.edu.gt>
2022-05-19 19:53:32 -0500

    Se agregaron las rutas de buscar productos

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -11,3 +11,10 @@ api.put('/updateProduct/:id', mdAuth.ensureAuth, productController.updateProduct
 api.get('/getProduct/:id', mdAuth.ensureAuth, productController.getProduct);
 api.get('/getProducts', mdAuth.ensureAuth, productController.getProducts);
 api.delete('/deleteProduct/:id', mdAuth.ensureAuth, productController.deleteProduct);
+//RUTAS DE BUSQUEDA DE PRODUCTOS
+api.get('/getProductsByStockAsc',mdAuth.ensureAuth, productController.getProductsByStockAsc);
+api.get('/getProductsByStockDesc',mdAuth.ensureAuth, productController.getProductsByStockDesc);
+api.get('/getProductsByNameAsc',mdAuth.ensureAuth, productController.getProductsByNameAsc);
+api.get('/getProductsByNameDesc',mdAuth.ensureAuth, productController.getProductsByNameDesc);
+api.get('/getProductsByProviderAsc',mdAuth.ensureAuth, productController.getProductsByProviderAsc);
+api.get('/getProductsByProviderDesc',mdAuth.ensureAuth, productController.getProductsByProviderDesc);
