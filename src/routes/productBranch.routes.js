edaf278f899cfc470a7f685558ca67400a40a8dc
Walter  Sactic <wsactic-2020156@kinal.edu.gt>
2022-05-19 20:29:20 -0600

    Se agregaron las rutas para agregar y vender productos a productBranch.

--- src/routes/productBranch.routes.js
+++ src/routes/productBranch.routes.js
@@ -5,10 +5,9 @@ const api = express.Router();
 const productBranchController= require('../controllers/productBranch.controller'); 
 const mdAuth = require('../services/authenticated');
 
-
-
-
-
+//---------------------Rutas para Agregar y vender Productos en Sucursales---------------
+api.post('/addProductsBranch/:enterpriseBranchId', mdAuth.ensureAuth, productBranchController.addProductBranch);
+api.post('/saleOfProduct/:enterpriseBranchId/:id', mdAuth.ensureAuth, productBranchController.saleOfProduct); 
 
 //----------------------Rutas para Buscar productos de Sucursal--------------------------
 api.get('/getProductBranch/:enterpriseBranchId/:id',mdAuth.ensureAuth, productBranchController.getProductBranch);
